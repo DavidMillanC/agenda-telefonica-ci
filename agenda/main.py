05e0e8b3def92db177fe48c2b2e7f44a4c92b3e6
rmaciasa6 <rmaciasa6@unemi.edu.ec>
2025-10-05 21:36:28 -0500

    Validaciones y coverage en html

--- agenda/main.py
+++ agenda/main.py
@@ -1,16 +1,32 @@
+import re  
 class AgendaTelefonica:
     def __init__(self):
         self.contactos = {}
 
-    def agregar_contacto(self, nombre, telefono):
-        if not nombre.isalpha():
-            raise ValueError("El nombre debe contener solo letras.")
-        if not telefono.isdigit() or len(telefono) != 10:
-            raise ValueError("El número debe tener exactamente 10 dígitos.")
-        self.contactos[nombre] = telefono
-
     def obtener_contacto(self, nombre):
-        return self.contactos.get(nombre, "Contacto no encontrado.")
+                if not self.contactos:
+                    return self.contactos.get(nombre, "Contacto no encontrado.")
+                return "\n".join([f"{nombre}: {telefono}" for nombre, telefono in self.contactos.items()])
 
     def buscar_contacto(self, filtro):
         return {nombre: tel for nombre, tel in self.contactos.items() if filtro.lower() in nombre.lower()}
+
+    def agregar_contacto(self, nombre, telefono):
+        nombre = nombre.strip()
+        if not re.fullmatch(r"[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+", nombre):
+            raise ValueError("El nombre debe contener solo letras y espacios.")
+        if not telefono.isdigit() or len(telefono) != 10:
+            raise ValueError("El número debe tener exactamente 10 dígitos.")
+        if nombre in self.contactos:
+            raise ValueError("El contacto ya existe.")
+        self.contactos[nombre] = telefono
+
+
+    def eliminar_contacto(self, nombre):
+        nombre = nombre.strip()
+        if nombre in self.contactos:
+            del self.contactos[nombre]
+            return f"Contacto '{nombre}' eliminado."
+        else:
+            return "Contacto no encontrado."
+
